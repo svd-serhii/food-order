4618edc012b653f997dba00e2d6072e91236ad25
Serhii Demchenko <svdemchenko@gmail.com>
2024-03-10 21:55:18 +0200

    add Modal, add Context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,19 @@ import React, { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 const Header = () => {
 	const cartCtx = useContext(CartContext);
+	const userProgressCtx = useContext(UserProgressContext);
 
 	const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
 		return totalNumberOfItems + item.quantity;
 	}, 0);
+
+	function handleShowCart() {
+		userProgressCtx.showCart();
+	}
 
 	return (
 		<header id="main-header">
@@ -18,7 +24,10 @@ const Header = () => {
 				<h1>Food order app </h1>
 			</div>
 			<nav>
-				<Button textOnly> Cart ({totalCartItems}) </Button>
+				<Button textOnly onClick={handleShowCart}>
+					{" "}
+					Cart ({totalCartItems}){" "}
+				</Button>
 			</nav>
 		</header>
 	);
